28a0aa06fadbcad6e4d5b217528b75bfce6e8c38
doomedducks21-bit <doomedducks21@gmail.com>
2026-04-27 18:06:20 +0100

    Update AmadeusCord_Bot.py

--- AmadeusCord_Bot.py
+++ AmadeusCord_Bot.py
@@ -38,8 +38,8 @@ async def rapeuser(ctx,user : discord.Member,):
              "https://cdn.discordapp.com/attachments/1468324270525649087/1497769482414985417/Steins3BGate.1024.648722.png?ex=69eeba07&is=69ed6887&hm=64e31ef931e34bed79e06e7ade873801fd408b69ce9c6cc3320745477f019961&"
              "https://cdn.discordapp.com/attachments/1468324270525649087/1497769559715877024/images.png?ex=69eeba1a&is=69ed689a&hm=d9962dad1979faa43691f59b393e9ff4c2a3537576c85e90d997c08379cc5bd7&",
              "https://cdn.discordapp.com/attachments/1468324270525649087/1497770128086138993/9aec868844b6750b2a465f7ab19ea1af.png?ex=69eebaa1&is=69ed6921&hm=36cda024d3414b310eac09305a87c18bf4f6bf3e9c918f0cc624576625af5343&",
-             "https://media.discordapp.net/attachments/1494361267341623346/1497767668252409876/20A8F41.JPG?ex=69eeb857&is=69ed66d7&hm=18215beefafefd0febabf66b083e29aee1170c6df0461fd5450495522e6c1a0d&=&format=webp&width=1503&height=845",
-             "https://cdn.discordapp.com/attachments/1494361267341623346/1497767735898144809/image.png?ex=69eeb867&is=69ed66e7&hm=146698342fef44431397ba20354e784adc082bae5027bdbb36e0c1e1a9c22204&",
+             "https://cdn.discordapp.com/attachments/1468324270525649087/1498369652731154513/20A8F41.png?ex=69f0e8fb&is=69ef977b&hm=ad14775dce06ff386400b213d7a4af89b40224b6a6c68fc7194815bd0d18f9f8&",
+             "https://cdn.discordapp.com/attachments/1468324270525649087/1498369496249929889/image.png?ex=69f0e8d6&is=69ef9756&hm=ba8d335179441eb892befd967783343f589f99bd9cdeb8914bb04a3d9d5ff809&",
              "https://cdn.discordapp.com/attachments/1468324270525649087/1497770218569728060/sample_b093fdd1c557f53de1b9333f67e036dd.png?ex=69eebab7&is=69ed6937&hm=1be5be4693cf5c9dd9efdd6f068019483d2612d0622f293ecaad10e10e5d1c3d&",
              "https://cdn.discordapp.com/attachments/1468324270525649087/1497770320843767918/sample_d7227183bb6a812571c427d51e6e9d8a.png?ex=69eebacf&is=69ed694f&hm=1700056314349448e414dfb601c699724cbb881b50a2ab69250272df1f292aa7&"
              ]
