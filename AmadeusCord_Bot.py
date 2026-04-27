9d878183846f99e1dde21152170f8b0a16331917
doomedducks21-bit <doomedducks21@gmail.com>
2026-04-27 18:03:15 +0100

    Update AmadeusCord_Bot.py

--- AmadeusCord_Bot.py
+++ AmadeusCord_Bot.py
@@ -32,9 +32,9 @@ async def banuser(ctx,user : discord.User):
 async def rapeuser(ctx,user : discord.Member,):
     rurls = ["https://cdn.discordapp.com/attachments/1468324270525649087/1497758693519917136/image.png?ex=69eeaffb&is=69ed5e7b&hm=b2a8c2d8c653d4b538fdd5daf16a76446613e84bec3fd0710a1737280ee81467&",
              "https://cdn.discordapp.com/attachments/1468324270525649087/1497758718107062393/image.png?ex=69eeb001&is=69ed5e81&hm=8c6c1bd271ce251a8f781ea92c0652bad733d95f14b156093e7a124146e60de1&",
-             "https://media.discordapp.net/attachments/1494361267341623346/1497764194655932457/2063441.JPG?ex=69eeb51b&is=69ed639b&hm=127dfd047a166ee76c807e477fd91232124ffcea706021dbdc614a9219ed3b33&=&format=webp&width=1503&height=845",
-             "https://cdn.discordapp.com/attachments/1494361267341623346/1497766000362258492/image.png?ex=69eeb6c9&is=69ed6549&hm=d39bcfff0c8cb4af88430db789f8bbc6431948feab835dc16667402fcd107ef4&",
-             "https://cdn.discordapp.com/attachments/1494361267341623346/1497766132319260763/image.png?ex=69eeb6e9&is=69ed6569&hm=ed211cfd3d862848357dd67b02a2b10c26101a547edf811d805304e0f05151dc&",
+             "https://cdn.discordapp.com/attachments/1468324270525649087/1498368522810818560/image.png?ex=69f0e7ee&is=69ef966e&hm=2dbb3011a33de781151a1383a92708e9e8da476c6c55854e273547f313d8e3f4&",
+             "https://tenor.com/view/chaos-head-anime-stop-it-dog-sena-aoi-gif-25432962",
+             "https://cdn.discordapp.com/attachments/502102780052504576/772485866739597322/subaru_junna.gif?ex=69f0d586&is=69ef8406&hm=dae7a444618056aeeb7b3ea680419c247a7767ee89371a8da563d96b5e387bee&",
              "https://cdn.discordapp.com/attachments/1468324270525649087/1497769482414985417/Steins3BGate.1024.648722.png?ex=69eeba07&is=69ed6887&hm=64e31ef931e34bed79e06e7ade873801fd408b69ce9c6cc3320745477f019961&"
              "https://cdn.discordapp.com/attachments/1468324270525649087/1497769559715877024/images.png?ex=69eeba1a&is=69ed689a&hm=d9962dad1979faa43691f59b393e9ff4c2a3537576c85e90d997c08379cc5bd7&",
              "https://cdn.discordapp.com/attachments/1468324270525649087/1497770128086138993/9aec868844b6750b2a465f7ab19ea1af.png?ex=69eebaa1&is=69ed6921&hm=36cda024d3414b310eac09305a87c18bf4f6bf3e9c918f0cc624576625af5343&",
